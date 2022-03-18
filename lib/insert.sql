baa0ce6cbc025974f6f780c6de25f2031fae2db2
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-18 15:24:05 -0400

    commit tree 006007de4750de0d1f2d87d780c25ec44bf305e5

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'brown', 'decadent', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'black and white', 'silly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 16, 'F', 'black', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 18, 'M', 'white', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'black', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 11, 'M', 'green', "anyone he's ever screwed over deserved it.", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 27, 'F', 'beige', 'bit bitchy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, 'F', 'black and white', NULL, 1);
